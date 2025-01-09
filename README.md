721d8a62ae1a1d4d85dc70fe269e4232ec8abcf7
Arbab Husain <arbabhusain200@gmail.com>
2025-01-09 18:27:02 -0500

    Added some test code

---
+++ README.md
@@ -0,0 +1,6 @@
+Soccer National Team Elo Engine
+
+Overview:
+This project is an application that calculates and tracks the Elo ratings over national soccer teams. With an elo engine implemented from scratch, this tool offers an accurate performance tracking and ranking over time
+
+Features:
---
+++ src/tests/testelo.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+
+from main import eloEngine
+
+# Mock data for testing purposes
+mock_data = [
+    {'home_team': 'Team A', 'away_team': 'Team B', 'home_result': 1, 'away_result': 0},  # Team A wins
+    {'home_team': 'Team C', 'away_team': 'Team D', 'home_result': 0, 'away_result': 1},  # Team D wins
+    {'home_team': 'Team A', 'away_team': 'Team C', 'home_result': 1, 'away_result': 0},  # Team A wins
+    {'home_team': 'Team B', 'away_team': 'Team D', 'home_result': 0, 'away_result': 1},  # Team D wins
+    {'home_team': 'Team A', 'away_team': 'Team D', 'home_result': 1, 'away_result': 1},  # Draw
+]
+
+
+df = pd.DataFrame(mock_data)
+
+unique_teams = np.unique(df[['home_team', 'away_team']])
+
+def runTests():
+    print("Running Elo Engine Test...\n")
+    teams_final_ratings = eloEngine()
+    teams_final_ratings.to_csv('tests/test.csv', index=False)
+    
+
+runTests()
